ce9df3e6092d1ac1d5fe769298eb3196e9e337c5
silkflo <florian.assous@hotmail.fr>
2019-05-02 04:02:37 +0700

    test,  can pull this commit

--- Assets/Scripts/Gameplay/SpawnFood.cs
+++ Assets/Scripts/Gameplay/SpawnFood.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class SpawnFood : MonoBehaviour
 {
     public static SpawnFood instance;
-
+      
     public float startPositionFoodX = -1f;
     public float startPositionFoodY = 20f;
 
